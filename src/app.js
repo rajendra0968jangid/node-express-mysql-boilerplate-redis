6170f086b22b05874e61c175f404d412a2232900
rajendra0968jangid <rajendra0968jangid@gmail.com>
2025-01-24 13:37:15 +0530

    test

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
 
 app.get('/', async (req, res) => {
-    res.status(200).send('Congratulations! API is working okay!');
+    res.status(200).send('Congratulations! API is working rajendra!');
 });
 app.use('/api', routes);
 
